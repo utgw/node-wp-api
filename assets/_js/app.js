612dc90daa20443ef0f4a5f9e419a9fc0314e9c3
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-07-10 16:16:57 +0900

    remove getPosts

--- assets/_js/app.js
+++ assets/_js/app.js
@@ -1,28 +1,6 @@
 module.exports = (function () {
   return {
     init: function (api) {
-      this.getPosts(api);
-    },
-    getPosts: function (api) {
-
-      $.ajax({
-        type: 'GET',
-        url: api,
-        dataType: 'json'
-      })
-      .done(function (data) {
-        $(data).each(function (index, value) {
-          $('ul.posts').append(
-            '<li class="post">' +
-              '<h2 class="entry-title">' + value.title.rendered + '</h2>' +
-              '<div class="entry-content">' + value.content.rendered + '</div>' +
-            '</li>'
-          );
-        });
-      })
-      .fail(function (error) {
-        console.log(error);
-      });
     }
   };
 })();
